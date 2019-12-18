812728dc93fb32c413a4afa68e60b4216e3fa494
ambujBhatnag <46799885+ambujBhatnag@users.noreply.github.com>
2019-12-18 10:57:07 -0800

    Add files via upload

---
+++ Greeter.java
@@ -0,0 +1,6 @@
+// uppercase letter for the first letter of class java doesn't like spaces
+public class Greeter {
+   public static void main( String[] args ) {
+      System.out.println( "Hello World" );
+   }
+}
